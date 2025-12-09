3b86087fe8aefa6f41d792dccc63c27b46ad3176
jersa <soyjuan935117@gmail.com>
2025-12-08 23:28:43 -0600

    crear clase Oportunidades con atributos y constructores

--- src/juego.java
+++ src/juego.java
@@ -1,4 +1,20 @@
 
+class Oportunidades{
+	
+	private int intentosRestantes;
+    private int maxIntentos;
+
+    public Oportunidades(int maxIntentos) {
+        this.maxIntentos = maxIntentos;
+        this.intentosRestantes = maxIntentos;
+    }
+	
+}// clase oportunidades
+
+
 public class juego {
+	public static void main(String[] args) {
+		
+	}// main
 
 }
